3d0f75186ae8d4d08797fb44ceb52efa73ca85f8
Jorge Vargas <jorge@maxxpotential.com>
2018-03-07 20:56:01 -0500

    Make am respond on any mention

--- am.py
+++ am.py
@@ -30,10 +30,13 @@ def parse_bot_commands(slack_events):
     """
     for event in slack_events:
         if event["type"] == "message" and not "subtype" in event:
-            user_id, message = parse_direct_mention(event["text"])
-            if user_id == starterbot_id:
-                return message, event["channel"]
+            message = parse_mention(event["text"])
+            return message, event["channel"]
     return None, None
+
+def parse_mention(message_text):
+    if "am" in message_text:
+        return message_text
 
 def parse_direct_mention(message_text):
     """
