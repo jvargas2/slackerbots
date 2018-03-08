bf8a1e9078afa1a41f6a8aaf4b7ebc875c188436
Jorge Vargas <jorge@maxxpotential.com>
2018-03-07 23:59:05 -0500

    Prepare for training

--- bots_direct.py
+++ bots_direct.py
@@ -24,15 +24,17 @@ chatbot = am
 
 learn_list = []
 
-def learn(message):
-    global learn_list
-    learn_list.append(message)
-    if len(learn_list) >= 5:
-        chatbot.train(learn_list)
-        learn_list = []
-    print(learn_list)
+# def learn(message):
+#     global learn_list
+#     learn_list.append(message)
+#     if len(learn_list) >= 5:
+#         chatbot.train(learn_list)
+#         learn_list = []
+#     print(learn_list)
 
-while True:
-    m = input("> ")
-    print(chatbot.get_response(m))
-    learn(m)
+# for c in learn_list:
+    # chatbot.train(c)
+
+# while True:
+#     m = input("> ")
+#     print(chatbot.get_response(m))
